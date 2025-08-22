aa376f0927452143f02a4bbdba892c0bcf14426a
ROJAS30 <160422779+ROJAS30@users.noreply.github.com>
2025-08-22 11:57:24 -0400

    agrega propiedad Email a Empleado

--- gestion-empleados-codigo-base/src/gestion-empleados/Models/Empleado.cs
+++ gestion-empleados-codigo-base/src/gestion-empleados/Models/Empleado.cs
@@ -6,5 +6,7 @@ namespace gestion_empleados.Models
         public string Nombre { get; set; } = string.Empty;
         public string Departamento { get; set; } = string.Empty;
         public decimal Salario { get; set; }
+        public string Email { get; set; } = string.Empty;
+
     }
 }
